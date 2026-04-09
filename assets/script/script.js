17e6c038468952535e39affd4460dbdf8c380cc1
Blake Thompson <blthomps3@gmail.com>
2026-04-09 00:48:08 -0500

    Set up important variables

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const start = document.getElementById('start-btn');
+const restart = document.getElementById('restart-btn');
+const wordinput = document.getElementById('word-input');
+
+const timer = document.getElementById('time');
+const displayWord = document.getElementById('current-word');
+const hits = document.getElementById('hits');
+
+const wordlist = ['dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building', 'population', 
+    'weather', 'bottle', 'history', 'dream', 'character', 'money', 'absolute', 'discipline', 'machine', 
+    'accurate', 'connection', 'rainbow', 'bicycle', 'eclipse', 'calculator', 'trouble', 'watermelon', 
+    'developer', 'philosophy', 'database', 'periodic', 'capitalism', 'abominable', 'component', 
+    'future', 'pasta', 'microwave', 'jungle', 'wallet', 'canada', 'coffee', 'beauty', 'agency', 
+    'chocolate', 'eleven', 'technology', 'alphabet', 'knowledge', 'magician', 'professor', 'triangle', 
+    'earthquake', 'baseball', 'beyond', 'evolution', 'banana', 'perfumer', 'computer', 'management', 
+    'discovery', 'ambition', 'music', 'eagle', 'crown', 'chess', 'laptop', 'bedroom', 'delivery', 
+    'enemy', 'button', 'superman', 'library', 'unboxing', 'bookstore', 'language', 'homework', 
+    'fantastic', 'economy', 'interview', 'awesome', 'challenge', 'science', 'mystery', 'famous', 
+    'league', 'memory', 'leather', 'planet', 'software', 'update', 'yellow', 'keyboard', 'window'];
